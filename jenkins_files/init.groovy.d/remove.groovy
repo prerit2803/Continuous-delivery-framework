b8853e22d27f28671c750b8e5a184bd9d2f3ef0f
pbhanda2 <pbhanda2@ncsu.edu>
2018-02-21 22:12:21 -0500

    Update remove.groovy

--- jenkins_files/init.groovy.d/remove.groovy
+++ jenkins_files/init.groovy.d/remove.groovy
@@ -1,5 +1,5 @@
 def groovySource = new File('/var/lib/jenkins/init.groovy.d/makeCred.groovy')
 //println groovySource.text
 //println "cred:"+cred
-groovySource.text= groovySource.text.replaceAll('(addPassword\\(.*)(,)(.*)(\\))','$1$2\'githubpwd\'$4')
+groovySource.text= groovySource.text.replaceAll('(addPassword\\()(\\w+)(,)(.*)(\\))','$1\'githubuser\'$3\'githubpwd\'$5')
 //println groovySource.text
