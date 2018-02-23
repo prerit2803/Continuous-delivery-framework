f31cea2da75758e1b9a0eabf2e07b420a2f7b94e
pbhanda2 <pbhanda2@ncsu.edu>
2018-02-23 05:38:48 +0000

    Modified makeCred for handling incorrect password

--- jenkins_files/init.groovy.d/makeCred.groovy
+++ jenkins_files/init.groovy.d/makeCred.groovy
@@ -7,8 +7,40 @@ import hudson.*
 import hudson.model.*
 import hudson.security.*
 import hudson.slaves.EnvironmentVariablesNodeProperty
-
+//import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl
 def instance= Jenkins.get()
+
+
+def changePassword = { username, new_password ->
+    def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+        com.cloudbees.plugins.credentials.common.StandardUsernameCredentials.class,
+        Jenkins.instance
+    )
+
+    def c = creds.findResult { it.username == username ? it : null }
+
+    if ( c ) {
+        println "found credential ${c.id} for username ${c.username}"
+
+        def credentials_store = Jenkins.instance.getExtensionList(
+            'com.cloudbees.plugins.credentials.SystemCredentialsProvider'
+            )[0].getStore()
+
+        def result = credentials_store.updateCredentials(
+            com.cloudbees.plugins.credentials.domains.Domain.global(),
+            c,
+            new UsernamePasswordCredentialsImpl(c.scope, c.id, c.description, c.username, new_password)
+            )
+
+        if (result) {
+            println "password changed for ${username}"
+        } else {
+            println "failed to change password for ${username}"
+        }
+    } else {
+      println "could not find credential for ${username}"
+    }
+}
 
 def addPassword = { username, new_password ->
     def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
@@ -20,6 +52,8 @@ def addPassword = { username, new_password ->
 
     if ( c ) {
         println "found credential ${c.id} for username ${c.username}"
+        println "Updating Password"
+        changePassword(username, new_password)
     } else {
         def credentials_store = Jenkins.instance.getExtensionList(
             'com.cloudbees.plugins.credentials.SystemCredentialsProvider'
@@ -35,7 +69,39 @@ def addPassword = { username, new_password ->
             )
 
         if (result) {
-            println "credential added for ${username}"
+            println "credential added for ${username}: ${result}"
+
+             def creds2 = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+        		com.cloudbees.plugins.credentials.common.StandardUsernameCredentials.class,
+        		Jenkins.instance
+    			)
+
+            def newCred = creds2.findResult { it.username == username ? it : null }
+
+            globalNodeProperties = instance.getGlobalNodeProperties()
+            envVarsNodePropertyList = globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class)
+
+            newEnvVarsNodeProperty = null
+            envVars = null
+
+            if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
+              newEnvVarsNodeProperty = new EnvironmentVariablesNodeProperty();
+              globalNodeProperties.add(newEnvVarsNodeProperty)
+              envVars = newEnvVarsNodeProperty.getEnvVars()
+            } else {
+              envVars = envVarsNodePropertyList.get(0).getEnvVars()
+            }
+
+            // Setting necessary environment varibales for build and deployment of iTrust and Checkbox.io
+          //println "Hello : ${newCred}"
+          envVars.put("GIT_CREDID",newCred.id )
+
+            def source = new File('/var/lib/jenkins/project_repo/jenkins_files/itrust.yml')
+            //println source.text
+            def cred= envVars.get('GIT_CREDID')
+            //println "cred:"+cred
+            source.text= source.text.replaceAll('gitCredId',cred)
+            //println source.text
         } else {
             println "failed to add credential for ${username}"
         }
@@ -43,43 +109,3 @@ def addPassword = { username, new_password ->
 }
 
 addPassword('githubuser','githubpwd')
-
-
-def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-      com.cloudbees.plugins.credentials.common.StandardUsernameCredentials.class,
-      Jenkins.instance,
-      null,
-      null
-  );
-for (c in creds) {
-println(c.id + ": " + c.description)
-//Set environment variables
-
-globalNodeProperties = instance.getGlobalNodeProperties()
-envVarsNodePropertyList = globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class)
-
-newEnvVarsNodeProperty = null
-envVars = null
-
-if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
-  newEnvVarsNodeProperty = new EnvironmentVariablesNodeProperty();
-  globalNodeProperties.add(newEnvVarsNodeProperty)
-  envVars = newEnvVarsNodeProperty.getEnvVars()
-} else {
-  envVars = envVarsNodePropertyList.get(0).getEnvVars()
-}
-
-// Setting necessary environment varibales for build and deployment of iTrust and Checkbox.io
-envVars.put("GIT_CREDID",c.id )
-
-def source = new File('/var/lib/jenkins/project_repo/jenkins_files/itrust.yml')
-//println source.text
-def cred= envVars.get('GIT_CREDID')
-//println "cred:"+cred
-source.text= source.text.replaceAll('gitCredId',cred)
-//println source.text
-
-
-
-
-}
--- jenkins_files/init.groovy.d/remove.groovy
+++ jenkins_files/init.groovy.d/remove.groovy
@@ -1,7 +1,3 @@
 // Removing Git Credentials from makeCred
 def groovySource = new File('/var/lib/jenkins/init.groovy.d/makeCred.groovy')
 groovySource.text= groovySource.text.replaceAll('(addPassword\\()(\\w+)(,)(.*)(\\))','$1\'githubuser\'$3\'githubpwd\'$5')
-
-// Removing MySQL Password from makeCred
-// def groovySource = new File('/var/lib/jenkins/init.groovy.d/makeCred.groovy')
-// groovySource.text= groovySource.text.replaceAll('(addPassword\\()(\\w+)(,)(.*)(\\))','$1\'githubuser\'$3\'githubpwd\'$5')
