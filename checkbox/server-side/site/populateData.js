23f0a12436f5b6cc7bcdbdf23763e6d5a0317ee5
pbhanda2 <pbhanda2@ncsu.edu>
2018-03-22 23:38:20 -0400

    Code cleaning

--- checkbox/server-side/site/populateData.js
+++ checkbox/server-side/site/populateData.js
@@ -10,11 +10,8 @@ var IDCollection = [];
 
 var study= Models.StudyModel;
 var vote= Models.VoteModel;
-// study.remove(function(err){
-//   if(err) console.log(err);
-// });
+
 let arr = populateData().then(function(coll){
-  // console.log("adsdasd",coll);
   module.exports.data = coll;
   process.exit(0);
 });
@@ -23,14 +20,7 @@ let arr = populateData().then(function(coll){
 function populateData(){
   return new Promise(function(resolve, reject){
     _.times(5, function(){
-      // console.log("CALLING ADD STUDY")
       addStudy().then(function(status){
-        // console.log(status);
-        // study.find(function (err, kittens) {
-        //   if (err) return console.error(err);
-        //   console.log(kittens);
-        // });
-        // console.log(IDCollection);
         resolve(IDCollection)
       }).catch(function(err){
         console.log(err);
@@ -40,16 +30,13 @@ function populateData(){
 }
 
 function addStudy(){
-  // console.log("In ADD STUDY")
   return new Promise(function(resolve, reject){
     getData().then(function(status){
-      // console.log("asdasd")
       resolve(status)
     });
   });
 }
 function getData(){
-  // console.log("In GET STUDY")
   return new Promise(function(resolve, reject){
     study.create({
     	"name" : faker.name.findName(),
@@ -72,116 +59,3 @@ function getData(){
     });
   });
 }
-
-
-// _.times(1, function(){
-//   console.log("CALLING ADD Vote")
-//   addVote().then(function(status){
-//     console.log(status);
-//     // study.find(function (err, kittens) {
-//     //   if (err) return console.error(err);
-//     //   console.log(kittens);
-//     // });
-//   }).catch(function(err){
-//     console.log(err);
-//   });
-// });
-//
-// function addVote(){
-//   console.log("In ADD vote")
-//   return new Promise(function(resolve, reject){
-//     getVote().then(function(status){
-//       resolve(status)
-//     });
-//   });
-// }
-// function getVote(){
-//   console.log("In GET vote")
-//   return new Promise(function(resolve, reject){
-//     vote.create({
-//       "studyId": mongoose.Types.ObjectId("5aaedee73742321912a4f482"),
-//       "fingerprint": "fingerprint-here",
-//       "answers":  {"ques1": "ans1", "ques2": "ans2"} ,
-//       "email": "user@ust.com",
-//       "contact": "1234567890"
-//     },function(error, doc) { //This saves the information you see within that Bee declaration (lines 4-6).
-//         if (error)
-//           reject(error);
-//         console.log(doc)
-//         vote.find(function (err, kittens) {
-//           if (err) return console.error(err);
-//           console.log(kittens);
-//         });
-//         resolve("saved");
-//     });
-//   });
-// }
-
-
-
-
-
-
-
-// console.log(study)
-// var stud = study.create({
-// // "name" : "aker.name.findName()",
-// // "description" : "faker.lorem.sentence()",
-// "studyKind" : "survey1",
-// // "researcherName" : "faker.name.findName()",
-// // "contact" : "1234567890",
-// "status" : "open",
-// // "goal" : _.random(1, 100),
-// "invitecode" : "NEQ-RESEARCH"
-// },function(error, doc) { //This saves the information you see within that Bee declaration (lines 4-6).
-//   if (error){
-//     console.log("ERROR HEREE", error);
-//     return;
-//   }
-//   console.log("SAVED")
-//   // done("SAVED");
-//   // resolve("saved");
-// });
-// study.find(function (err, kittens) {
-//   if (err) return console.error(err);
-//   console.log(kittens);
-// });
-//
-// function abc(done){
-//   var study= Models.StudyModel;
-//   console.log(study)
-// study.create({
-//   // "name" : "aker.name.findName()",
-//   // "description" : "faker.lorem.sentence()",
-//   "studyKind" : "survey1",
-//   // "researcherName" : "faker.name.findName()",
-//   // "contact" : "1234567890",
-//   "status" : "open",
-//   // "goal" : _.random(1, 100),
-//   "invitecode" : "RESEARCH"
-// },function(error, doc) { //This saves the information you see within that Bee declaration (lines 4-6).
-//     if (error){
-//       console.log("ERROR HEREE", error);
-//       return;
-//     }
-//     console.log("SAVED")
-//     done("SAVED");
-//     // resolve("saved");
-// });
-//
-// }
-
-// abc(function(save){
-//   console.log("from done", save);
-// });
-// function saveData(Study){
-//   console.log("In SAVE STUDY")
-//   return new Promise(function(resolve, reject){
-//     Study.save(function(error) { //This saves the information you see within that Bee declaration (lines 4-6).
-//       resolve("saved.");
-//       if (error) {
-//         reject(error);
-//       }
-//     });
-//   });
-// }
