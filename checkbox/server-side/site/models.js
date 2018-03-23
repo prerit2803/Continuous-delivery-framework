fbc5372ebb263dd7da65983d8cf4a6406276f9c2
pbhanda2 <pbhanda2@ncsu.edu>
2018-03-22 23:34:01 -0400

    Code cleaning

--- checkbox/server-side/site/models.js
+++ checkbox/server-side/site/models.js
@@ -7,15 +7,10 @@ var mongoose = require('mongoose');
     mongoose.model('StudyModel', require('./studySchema'), 'studies');
   var VoteModel =
     mongoose.model('VoteModel', require('./voteSchema'), 'votes');
-  // var User =
-  //   mongoose.model('User', require('./user'), 'users');
 
   var models = {
     StudyModel: StudyModel,
     VoteModel: VoteModel
-    //User: User
   };
-
-// console.log("in Models", models)
 
   module.exports.models=models;
