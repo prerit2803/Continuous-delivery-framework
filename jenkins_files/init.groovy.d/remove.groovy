4a2f40b256ddc6be09b380752d6e5f29ed5f342a
Zach Thomas <zacherythomas12@gmail.com>
2018-04-11 22:32:16 -0400

    Cleanup from milestone2

--- jenkins_files/init.groovy.d/remove.groovy
+++
@@ -1,3 +0,0 @@
-// Removing Git Credentials from makeCred
-def groovySource = new File('/var/lib/jenkins/init.groovy.d/makeCred.groovy')
-groovySource.text= groovySource.text.replaceAll('(addPassword\\()(\\w+)(,)(.*)(\\))','$1\'githubuser\'$3\'githubpwd\'$5')
