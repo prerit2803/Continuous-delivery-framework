6883595c382cf7c557576bbc936fc1beaa9253d7
Zach Thomas <zacherythomas12@gmail.com>
2018-04-13 20:26:47 -0400

    Updates for jjb and pipeline_testers

--- Deployment/pipeline_test_scripts/iTrust_pipeline_tester.sh
+++ Deployment/pipeline_test_scripts/iTrust_pipeline_tester.sh
@@ -3,7 +3,7 @@
 jenkins_port=`python ymlparse.py 'jenkins_port'`
 GIT_USER=`python ymlparse.py 'GIT_USER'`
 GIT_PASSWORD=`python ymlparse.py 'GIT_PASSWORD'`
-JOB_URL=http://localhost:`echo $jenkins_port`/job/itrust-build
+JOB_URL=http://localhost:${jenkins_port}/job/itrust-build
 JOB_STATUS_URL=${JOB_URL}/lastBuild/api/json
 GREP_RETURN_CODE=0
 
@@ -15,7 +15,7 @@ pushd .
 
 cd /var/lib/jenkins
 
-sudo git clone https://`echo $GIT_USER`:`echo $GIT_PASSWORD`@github.ncsu.edu/engr-csc326-staff/iTrust2-v2.git
+sudo git clone https://${GIT_USER}:${GIT_PASSWORD}@github.ncsu.edu/engr-csc326-staff/iTrust2-v2.git
 
 sudo mkdir iTrust.git
 cd iTrust.git
@@ -23,7 +23,7 @@ sudo git init --bare
 
 cd $HOME
 echo """#!/bin/bash
-curl 'http://localhost:`echo $jenkins_port`/git/notifyCommit?url=/var/lib/jenkins/iTrust.git'""" >> post-receive
+curl 'http://localhost:${jenkins_port}/git/notifyCommit?url=/var/lib/jenkins/iTrust.git'""" >> post-receive
 
 sudo chmod 777 post-receive
 sudo mv post-receive /var/lib/jenkins/iTrust.git/hooks
@@ -38,15 +38,7 @@ sudo git add README.md
 sudo git commit -m 'test'
 sudo git push prod master
 
-# Poll every couple of seconds until the build is finished
-while [ $GREP_RETURN_CODE -eq 0 ]
-do
-    sleep 10
-    # Grep will return 0 while the build is running:
-    curl --silent $JOB_STATUS_URL | grep result\":null > /dev/null
-    GREP_RETURN_CODE=$?
-done
-# from https://serverfault.com/questions/309848/how-to-check-the-build-status-of-a-jenkins-build-from-the-command-line
+sleep 30
 
 sudo rm -rf /var/lib/jenkins/iTrust2-v2
 sudo rm -rf /var/lib/jenkins/iTrust.git
