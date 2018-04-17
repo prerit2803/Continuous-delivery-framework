3a9fa76cbe94391c41199144887792d1b821861a
Zach Thomas <zacherythomas12@gmail.com>
2018-04-16 21:06:59 -0400

    Added modified checkbox code that supports feature flags

---
+++ Infrastructure/checkbox/server-side/site/routes/create.js
@@ -0,0 +1,121 @@
+var mongo = require('mongodb');
+var crypto = require('crypto');
+var emailjs = require('emailjs/email');
+var models = require('./studyModel.js');
+var redis  = require('redis');
+ 
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+ 
+var MongoClient = mongo.MongoClient;
+var db = null;
+MongoClient.connect("mongodb://"+process.env.MONGO_USER+":"+process.env.MONGO_PASSWORD+"@"+process.env.MONGO_IP+":27017/site?authSource=admin", function(err, authdb) {
+  // Now you can use the database in the db variable
+  db = authdb;
+  console.log( err || "connected!" );
+});
+
+var emailServer  = emailjs.server.connect({
+   user:    process.env.MAIL_USER, 
+   password:process.env.MAIL_PASSWORD, 
+   host:    process.env.MAIL_SMTP, 
+   ssl:     true,
+
+});
+
+exports.createStudy = function(req, res) {
+
+    var client = redis.createClient(6379, 'redisslave', {})
+
+    client.get('createStudy', function(err, value){
+        console.log('\n\nflag createStudy:', value, '\n\n')
+        if (value == "false" || value == null){
+            res.send({'error':'Feature not available'});
+            return;
+        }
+
+        var invitecode = req.body.invitecode; 
+        var studyKind = req.body.studyKind;
+    
+        if( invitecode != "RESEARCH" )
+        {
+            res.send({'error':'Invalid invitecode'});
+            return;
+        }
+    
+        basicCreate( req, res, studyKind ).onCreate( function(study)
+        {
+            db.collection('studies', function(err, collection) 
+            {
+                if( err )
+                    console.log( err );
+    
+                collection.insert(study, {safe:true}, function(err, result) 
+                {
+                    console.log( err || "Study created: " + study._id );
+    
+                    if( err )
+                    {
+                        res.send({error: err });
+                    }
+                    else
+                    {
+                        study.setPublicLink( study._id );
+    
+                        // update with new public link, and notify via email, redirect user to admin page.
+                        collection.update( {'_id' : study._id}, {'$set' : {'publicLink' : study.publicLink}},
+                            function(err, result )
+                        {
+                            sendStudyEmail( study );
+                            res.send({admin_url: study.adminLink});
+                        });
+                    }
+                });
+    
+            });
+        });
+    })
+};
+
+
+
+function basicCreate(req, res, kind) 
+{
+	console.log( kind );
+    this.onCreate = function ( onReady )
+    {
+	    crypto.randomBytes(48, function(ex, buf) 
+	    {
+	        // alternative: https://github.com/broofa/node-uuid
+	        var token = buf.toString('hex');
+
+	        console.log( token );
+
+	        var study = null;
+	        if( kind == "survey")
+	        {
+	        	study = new models.SurveyModel( req.body, token );
+	        }
+	        if( kind == "dataStudy")
+	        {
+	        	study = new models.DataStudyModel( req.body, token );
+	        }
+
+	        console.log( study );
+
+	        onReady(study);
+	    });
+	};
+
+	return this;
+}
+
+function sendStudyEmail (study) {
+    emailServer.send( study.getMessage(), 
+        function(err, message) 
+        { 
+            console.log(err || message); 
+        }
+    );
+}
